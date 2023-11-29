fbe3f975d4cf1b6715eee62750941a081be3c65a
eduardobr33 <100405421+eduardobr33@users.noreply.github.com>
2023-11-29 12:37:04 +0100

    Pathfinding

--- Game/Source/Map.h
+++ Game/Source/Map.h
@@ -4,7 +4,7 @@
 #include "Module.h"
 #include "List.h"
 #include "Point.h"
-//#include "Pathfinding.h"
+#include "Pathfinding.h"
 
 #include "PugiXml\src\pugixml.hpp"
 
@@ -166,6 +166,7 @@ private:
 public: 
 
 	MapData mapData;
+	PathFinding* pathfinding;
 
 private:
 
